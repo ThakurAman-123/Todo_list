cc3418103d9afdc33926f75fc74cfc7170352fdb
ThakurAman-123 <92178825+ThakurAman-123@users.noreply.github.com>
2022-08-13 16:30:56 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>you can store needy things<br></br>
+            you can add things<br></br>
+            Daily work routine<br></br>
+            Birthday date<br></br>
+            parties
+             </p>
+        </div>
+    )
+}
